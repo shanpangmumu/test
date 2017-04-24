a5f28babfda733e45145d139b8ef8958738d95ed
zhangqi <296493868@qq.com>
2017-04-24 16:38:46 +0800

    first time

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+import { DatePicker } from 'antd';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!
+        </div>
+        <DatePicker/>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
